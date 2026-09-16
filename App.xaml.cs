1e52e54fc5bec607ab025e35e083cb4a586eb6e7
benyypandes-coder <benyypandes@gmail.com>
2026-09-17 00:44:04 +0900

    Delete App.xaml.cs

--- App.xaml.cs
+++
@@ -1,7 +0,0 @@
-namespace KurtDhylanMotoShopInventory;
-public partial class App : Application
-{
-    public App() => InitializeComponent();
-    protected override Window CreateWindow(IActivationState? activationState)
-        => new Window(new AppShell());
-}
